ba72a01ae796f56e67960ef64637bead7a70b375
Ravi singh chaunal <ravichaunal30900@gmail.com>
2022-01-16 16:36:37 +0530

    my website
    gaming website  i will more impove it on future

---
+++ index.js
@@ -0,0 +1,40 @@
+function firstpara(){
+    let btn = document.getElementById('first');
+    let para = document.getElementById('para');
+if(para.style.display != 'none'){
+    para.style.display='none';
+}
+else{
+    para.style.display='block';
+}
+}
+function secondpara(){
+    let btn = document.getElementById('second');
+    let para = document.getElementById('para2');
+if(para.style.display != 'none'){
+    para.style.display='none';
+}
+else{
+    para.style.display='block';
+}
+}
+function thirdpara(){
+    let btn = document.getElementById('third');
+    let para = document.getElementById('para3');
+if(para.style.display != 'none'){
+    para.style.display='none';
+}
+else{
+    para.style.display='block';
+}
+}
+function fourthpara(){
+    let btn = document.getElementById('four');
+    let para = document.getElementById('para4');
+if(para.style.display != 'none'){
+    para.style.display='none';
+}
+else{
+    para.style.display='block';
+}
+}
